853db3db9d1098199ad2d1515423762105b17bf8
Esra Selin Akgül <73980419+esrselin@users.noreply.github.com>
2025-09-17 21:08:15 +0300

    Add click event, discount calc & fix negatives
    - Added click event listener to each product card that opens the product page in a new tab.
    - Implemented discount calculation when price and original_price differ.
    - Display both old and new prices along with the discount percentage.
    - Fixed an issue where discount could appear as a negative value by swapping price and original_price when needed.

--- ESRA_AKGUL.js
+++ ESRA_AKGUL.js
@@ -1,8 +1,7 @@
- if(window.location.pathname !== "/")
-    console.log("wrong page")
+if(window.location.pathname !== "/")
+  console.log("wrong page")
 else
 {
-
 
 function getData(url) {
   fetch(url)
@@ -13,9 +12,19 @@ function getData(url) {
         if (item.price === item.original_price) {
           priceHTML = `<span class="new-price">${item.price} TL</span>`;
         } else {
+            let discount = ((item.original_price - item.price)/item.original_price)*100;
+            if(discount < 0)
+            {
+                let temp;
+                temp = item.original_price;
+                item.original_price = item.price;
+                item.price = temp;
+                discount = -discount;
+            }
           priceHTML = `
             <span class="old-price">${item.original_price} TL</span>
             <span class="new-price">${item.price} TL</span>
+            <span class="discount">%${discount.toFixed(0)}</span>
           `;
         }
 
