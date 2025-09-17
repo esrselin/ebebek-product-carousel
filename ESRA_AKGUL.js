cf68ae4f42bdb89ed6d4de1f861fd1b64bbf39a0
Esra Selin Akgül <73980419+esrselin@users.noreply.github.com>
2025-09-17 18:11:32 +0300

    Add basic product fetch and render
    Implemented fetch to load products from JSON and render
    them into the carousel-container as basic item cards.

--- ESRA_AKGUL.js
+++ ESRA_AKGUL.js
@@ -1,17 +1,42 @@
-
  if(window.location.pathname !== "/")
-    console.log("wrong page")
-else
-{
+     console.log("wrong page")
+ else
+ {
 
-function getData(url)
-{
-    fetch(url)
+function getData(url) {
+  fetch(url)
     .then((respose) => respose.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
+    .then((data) =>
+      data.forEach((item) => {
+        const card = `
+          <div class="carousel-item">
+            <img src="${item.img}" alt="Product Image"/>
+            <p><strong>${item.brand}</strong> - ${item.name}</p>
+            <p>${item.price} TL</p>
+          </div>
+        `;
+
+        document.querySelector('.carousel-container').innerHTML += card;
+      })
+    );
 }
 
-getData('https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json')
+const carouselBar = document.createElement('div');
+const title = document.createElement("h2");
+title.textContent = "Beğenebileceğinizi düşündüklerimiz";
+carouselBar.appendChild(title);
+
+carouselBar.id = 'ebebek-carousel';
+
+carouselBar.innerHTML = `
+  <h2>Beğenebileceğinizi düşündüklerimiz</h2>
+  <div class="carousel-container"></div>
+`;
+
+document.body.appendChild(carouselBar);
+
+getData(
+  'https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json'
+);
 
 }
